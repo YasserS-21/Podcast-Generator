b18314c993582f5e2829425d88d0478b9b85fd33
YasserS <56772311+YasserS-21@users.noreply.github.com>
2025-02-04 23:53:54 -0500

    fix: align PodcastGenerator and PodcastDisplay for consistent centering; make PodcastPlayer sticky

--- front/src/components/PodcastDisplay/PodcastDisplay.tsx
+++ front/src/components/PodcastDisplay/PodcastDisplay.tsx
@@ -4,11 +4,13 @@ import ScriptDisplay from "./ScriptDisplay/ScriptDisplay"
 export default function PodcastDisplay({podcastData}) {
     
   return (
-    <>
-    <div className='podcast-display'>PodcastDisplay
+    <div className='podcast-display'>
+      <div className='podcast-player-container'>
         <PodcastPlayer podcastData={podcastData}/>
+      </div>
+      <div className='script-display-container'>
         <ScriptDisplay podcastData={podcastData} />
+      </div>
     </div>
-    </>
   )
 }
